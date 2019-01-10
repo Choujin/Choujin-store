1bd1191f7dadaee52ac744ba1b9892c3753f213c
Choujin <45816546+Choujin@users.noreply.github.com>
2019-01-10 23:37:53 +0100

    My first project of making a webshop

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Game Store</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/style.css">
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css.">
+    <script src="main.js"></script>
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
+
+  
+    
+</head>
+<body>
+    
+    <div id="top">      <!-- Top Begin -->
+
+        <div class="container">     <!-- Container Begin -->
+        
+        <div class="col-md-6 offer">        <!--col-md-6 offer Begin -->
+        
+            <a href="#" class="btn btn-succes btn-sm">Welcome</a>
+            <a href="checkout.php">4 Items In Your Cart | Total Price: $300</a>
+        
+        
+        </div>      <!--col-md-6 offer Finish -->
+
+        <div class="col-md6">       <!--col-md-6 offer Begin -->
+
+        <ul class="menu">        <!--menu Begin -->
+        
+            <li>
+                <a href="customer_register.php">Register</a>
+            </li>
+            <li>
+                <a href="checkout.php">My Account</a>
+            </li>
+            <li>
+                <a href="cart.php">Go To Cart</a>
+            </li>
+            <li>
+                <a href="checkout.php">Login</a>
+            </li>
+        
+        </ul>       <!--menu Finish -->
+        
+        
+        </div>      <!--col-md-6 offer Finish -->
+
+        </div>      <!-- Container Finish -->
+
+    </div>      <!-- Top Finish -->
+
+    <div id="navbar" class="navbar navbar-default">     <!-- navbar navbar-default Begin -->
+    
+    <div class="container">      <!-- container Begin -->
+    
+    <div class="navbar-header">     <!-- navbar-header Begin -->
+
+    <img src="images/ecom-store-logo.jpg" alt="Game Store Logo" class="hidden-xs">
+    <img src="images/ecom-store-logo-mobile.jpg" alt="Game Store Logo Mobile" class="visible-xs">
+
+    <a href="index.php" class="navbar-brand home">      <!-- navbar-brand home Begin -->
+    
+    
+    </a>        <!-- navbar-brand home Finish -->
+    
+    </div>      <!-- navbar-header Finish -->
+    
+    </div>      <!-- container Finish -->
+    
+    </div>      <!-- navbar navbar-default Finish -->
+
+</body>
+</html>
+
+
+
+
+
